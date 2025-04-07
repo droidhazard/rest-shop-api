aaccb0dc36dd49ee48f2eacb43307b1b1cf5b958
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-04-08 00:54:07 +0500

    'update' category

--- server/routers/categories.js
+++ server/routers/categories.js
@@ -11,6 +11,17 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  const foundCategory = await Category.findById(req.params.id);
+  if (foundCategory) {
+    res.send(foundCategory);
+  } else {
+    res
+      .status(404)
+      .send({ success: false, message: "category with this id not found" });
+  }
+});
+
 router.post("/", async (req, res) => {
   let newCategory = new Category({
     name: req.body.name,
@@ -22,6 +33,24 @@ router.post("/", async (req, res) => {
     res.status(500).json({ sucesss: false });
   } else {
     res.send(category);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const updatedCategory = await Category.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    },
+    { new: true }
+  );
+
+  if (updatedCategory) {
+    res.send(updatedCategory);
+  } else {
+    res.status(500).json({ success: false });
   }
 });
 
