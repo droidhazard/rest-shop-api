5f762c28dc30217b68d654b402746b09f0fb2955
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-04-07 11:35:17 +0500

    advanced 'product' schema

--- server/models/product.js
+++ server/models/product.js
@@ -1,9 +1,57 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: Number,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rickDescription: {
+    type: String,
+    default: `<p>This is where it all begin<p>`,
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [{ type: String }],
+  brand: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Schema",
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // exports.Product = mongoose.model("Product", productSchema);
