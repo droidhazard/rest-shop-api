12acad121f698b5ac557d2607a2f44ad1f6bdc47
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-04-08 00:24:01 +0500

    create category route

--- server/routers/categories.js
+++ server/routers/categories.js
@@ -11,4 +11,18 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  let newCategory = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+  let category = await newCategory.save();
+  if (!category) {
+    res.status(500).json({ sucesss: false });
+  } else {
+    res.send(category);
+  }
+});
+
 module.exports = router;
