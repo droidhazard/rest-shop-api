0ab7f98b48539c0480b9d8fc6c7d862d405e4a91
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-04-08 23:12:55 +0500

    product: update and delete routes, object id validation in PUT request

--- server/routers/products.js
+++ server/routers/products.js
@@ -2,13 +2,15 @@ const Category = require("../models/category.js");
 const Product = require("../models/product.js");
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 // const getProducts = router.get("/");
 
 // const createProduct = router.post("/");
 
 router.get(`/`, async (req, res) => {
-  const productsList = await Product.find().select("name image -_id");
+  // const productsList = await Product.find().select("name image -_id");
+  const productsList = await Product.find();
   if (!productsList) {
     res.status(500).json({ success: false });
   }
@@ -47,6 +49,48 @@ router.post(`/`, async (req, res) => {
   } else {
     res.status(500).json({ success: false });
   }
+});
+
+router.put("/:id", async (req, res) => {
+  const isValidId = mongoose.isValidObjectId(req.params.id);
+  if (isValidId) {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.description,
+        image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+      },
+      { new: true }
+    );
+
+    if (updatedProduct) {
+      res.send(updatedProduct);
+    } else {
+      res.status(500).json({ success: false });
+    }
+  } else {
+    res.status(400).json({ message: "invalid object id" });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const productId = req.params.id;
+  Product.findByIdAndDelete(productId)
+    .then((deletedProduct) => {
+      res.send(deletedProduct);
+    })
+    .catch((error) => {
+      res.status(400).json({ success: false });
+    });
 });
 
 module.exports = router;
