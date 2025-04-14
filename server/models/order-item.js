55b20c54cb98870bc9fc44c42531489eee6a4746
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-04-15 00:00:59 +0500

    order & line-items: model/schema

---
+++ server/models/order-item.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const LineItemsSchema = mongoose.Schema({
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("LineItems", LineItemsSchema);
--- server/models/order.js
+++ server/models/order.js
@@ -1,5 +1,60 @@
 const mongoose = require("mongoose");
+const LineItems = require("./order-item");
 
-const orderSchema = new mongoose.Schema({});
+const orderSchema = new mongoose.Schema({
+  lineItems: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "LineItems",
+      required: true,
+    },
+  ],
+  shippingAddress1: {
+    type: String,
+    required: true,
+  },
+  shippingAddress2: {
+    type: String,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "Pending",
+  },
+  totalPrice: {
+    type: Number,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  dateOrdered: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+orderSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+orderSchema.set("toJson", {
+  virtuals: true,
+});
 
 module.exports = mongoose.model("Order", orderSchema);
