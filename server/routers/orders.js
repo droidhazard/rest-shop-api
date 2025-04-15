c41f7d8b193e78baa41439d2593e8746fd102506
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-04-15 10:30:37 +0500

    order: create & get routes

--- server/routers/orders.js
+++ server/routers/orders.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Order = require("../models/order");
+const LineItem = require("../models/order-item");
 const authJwt = require("../helpers/jwt");
 
+// * GET ALL ORDERS
 router.get("/", async (req, res) => {
   const orderList = await Order.find();
   if (!orderList) {
@@ -12,10 +14,47 @@ router.get("/", async (req, res) => {
   }
 });
 
+// * GET ORDER BY ID
+router.get("/:id", async (req, res) => {
+  const order = await Order.findById(req.params.id).populate("lineItems user");
+  console.log(order);
+  if (order) {
+    res.send(order);
+  } else {
+    res.status(500).json({ success: false });
+  }
+});
+
+// * CREATE AN ORDER
 router.post("/", authJwt(), async (req, res) => {
-  const newOrder = req.body;
-  const createdOrder = await Order.create(newOrder);
-  res.status(201).json(createdOrder);
+  const lineItemIds = Promise.all(
+    req.body.lineItems.map(async (item) => {
+      let newLineItem = new LineItem({
+        quantity: item.quantity,
+        product: item.product,
+      });
+      newLineItem = await newLineItem.save();
+      return newLineItem._id;
+    })
+  );
+  const lineItemIdsResolved = await lineItemIds;
+  // console.log(lineItemIdsResolved);
+  let order = new Order({
+    lineItems: lineItemIdsResolved,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: req.body.totalPrice,
+    user: req.body.user,
+    dateOrdered: req.body.dateOrdered,
+  });
+
+  order = await order.save();
+  res.status(201).json(order);
 });
 
 module.exports = router;
