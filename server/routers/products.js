a9dc321be47dde2f50314699ad6b4915306888d6
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-04-18 23:39:12 +0500

    image: single image upload in products

--- server/routers/products.js
+++ server/routers/products.js
@@ -3,11 +3,36 @@ const Product = require("../models/product.js");
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const multer = require("multer");
 const authJwt = require("../helpers/jwt.js");
 
-// const getProducts = router.get("/");
+// * FILE UPLOAD STUFF
 
-// const createProduct = router.post("/");
+const FILE_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error("invalid upload type");
+    if (isValid) {
+      uploadError = null;
+    }
+    // cb(null, "public/uploads");
+    cb(uploadError, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const extension = FILE_TYPE_MAP[file.mimetype] || "jpg";
+    const fileName = file.originalname.split(" ").join("-");
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${fileName}_${Date.now()}.${extension}`);
+  },
+});
+
+const uploadOptions = multer({ storage: storage });
 
 // * GET ALL PRODUCTS
 router.get(`/`, authJwt(), async (req, res) => {
@@ -23,6 +48,7 @@ router.get(`/`, authJwt(), async (req, res) => {
   res.send(productsList);
 });
 
+// * GET PRODUCT BY ID
 router.get(`/:id`, async (req, res) => {
   const product = await Product.findById(req.params.id).populate("category");
   if (!product) {
@@ -32,7 +58,11 @@ router.get(`/:id`, async (req, res) => {
 });
 
 // * CREATE A PRODUCT
-router.post(`/`, authJwt(), async (req, res) => {
+router.post(`/`, authJwt(), uploadOptions.single("image"), async (req, res) => {
+  // * Uploaded file naming
+  const fileName = req.file.filename;
+  const basePath = `${req.protocol}://${req.get("host")}/public/uploads/`;
+
   const category = await Category.findById(req.body.category);
   if (!category) {
     res.status(400).json({ message: "invalid category" });
@@ -41,7 +71,7 @@ router.post(`/`, authJwt(), async (req, res) => {
     name: req.body.name,
     description: req.body.description,
     richDescription: req.body.description,
-    image: req.body.image,
+    image: `${basePath}${fileName}`,
     brand: req.body.brand,
     price: req.body.price,
     category: req.body.category,
@@ -58,6 +88,7 @@ router.post(`/`, authJwt(), async (req, res) => {
   }
 });
 
+// * UPDATE A PRODUCT
 router.put("/:id", authJwt(), async (req, res) => {
   const isValidId = mongoose.isValidObjectId(req.params.id);
   if (isValidId) {
@@ -89,6 +120,7 @@ router.put("/:id", authJwt(), async (req, res) => {
   }
 });
 
+// * DELETE A PRODUCT
 router.delete("/:id", authJwt(), (req, res) => {
   const productId = req.params.id;
   Product.findByIdAndDelete(productId)
@@ -100,12 +132,14 @@ router.delete("/:id", authJwt(), (req, res) => {
     });
 });
 
+// * GET TOTAL PRODUCTS COUNT
 router.get("/get/count", async (req, res) => {
   const productCount = await Product.countDocuments();
   res.send({ count: productCount });
   console.log(productCount);
 });
 
+// * LIST OF FEATURED PRODUCTS
 router.get("/get/featured/:limit", async (req, res) => {
   const limit = req.params.limit || 3;
   const featuredProducts = await Product.find({ isFeatured: true }).limit(
